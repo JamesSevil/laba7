791cc7a1173a982248ee233ebd252b23ac9d37d0
JamesSevil <jsclassified24@gmail.com>
2024-05-19 18:09:20 +0700

    Задание 2

--- zad2.cpp
+++ zad2.cpp
@@ -1,71 +1,83 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <vector>
 
 using namespace std;
 
-int evaluateExpression(string &s, int &i);
+int Expression(string &s, int &i);
 
 int calculate(string &s) {
     int i = 0;
-    return evaluateExpression(s, i);
+    return Expression(s, i);
 }
 
-int evaluateExpression(string &s, int &i) {
+int Expression(string &s, int &i) {
     int result = 0;
-    int sign = 1;  // 1(+)|-1(-)
     int num = 0;
- 
+    int sign = 1;
+
+    string C;
+
     while (i < s.length()) {
 
         char c = s[i];
 
-        if (isdigit(s[i])) { // Если число, то переводим его в int и записываем в переменную num
-            num += atoi(&c);
-        } else if (s[i] == '+') { // Если +, то записываем число в результат с соответствующим знаком, знак обновили
-            result += sign * num;
-            num = 0;
-            sign = 1;
-        } else if (s[i] == '-') { // Если -, то записываем число в результат с соответствующим знаком, знак обновили
-            result += sign * num;
-            num = 0;
+        if (isdigit(c)) { // Если символ - число, то добавляем его в строку и переводим в int
+            C += c;
+            num = stoi(C);
+        } else if ((c == '-' && i == 0) || (c == '-' && s[i+1] == '(')) { // цикл для корректной работы калькулятора( - в самом начале, - перед '(' )
             sign = -1;
-        } else if (s[i] == '(') { // Если встречаем (, то переходим на следующий элемент и считаем значения в скобках через рекурсию. Она остановится на )
-            i++;
-            int nestedResult = evaluateExpression(s, i);
-            result += sign * nestedResult;
-            num = 0;
-        } else if (s[i] == ')') { // Если встретили ), то записываем результат и возращаем его значения(для рекурсии)
+        } else if (c == '+' && s[i+1] == '(') { // цикл для корректной работы калькулятора( + перед '(' )
+            sign = 1;
+        } else if (c == '+') { // Если +, то...
+            num = stoi(C); // переводим число в строку(ещё раз, чтобы он считал также числа, состоящие из 2+ цифр)
+            C = ""; // обнуляем строку
+            result += sign * num; // записываем результат
+            num = 0; // обнуляем промежуточную переменную num
+            sign = 1; // обновляем знак
+        } else if (c == '-') { // Если -, то повторяем действия как с +, только меняем знак
+            num = stoi(C);
+            C = ""; 
+            result += sign * num; 
+            num = 0; 
+            sign = -1; 
+        } else if (c == '(') { // Если (, то запускаем рекурсию и высчитываем значение в скобках
+            i++; // переход к следующему элементу
+            int resultInSkobka = Expression(s, i); // рекурсия
+            result += sign * resultInSkobka; // значение рекурсии добавляем в общий результат
+        } else if (c == ')') { // Если ), то рекурсия заканчивается и записывается результат рекурсии
             result += sign * num;
             return result;
         }
-        i++; // переход на следующий элемент строки
+        
+        i++;
     }
 
     return result + sign * num;
 }
 
 int main() {
-    string expression;
+    setlocale(LC_ALL, "Rus");
+
+    string expression; // выражение
     while (true) {
         bool check = true;
         cout << "Введите выражение: ";
         cin >> expression;
-        if (expression.length()>3*(10^5)) { // проверка длины выражения
-            cout << "Слишком мало или много аргументов у выражения!" << endl;
+        if (expression.length()>3*1e5) { // проверка длины выражения
+            cout << "Слишком много аргументов у выражения!" << endl;
         } else {
-            int i = 0;
-            while (i < expression.length()) { // проверка на правильный ввод аргументов
-                if (!isdigit(expression[i]) && expression[i] != '+' && expression[i] != '-' && expression[i] != '(' && expression[i] != ')') {
+            int j = 0;
+            while (j < expression.length()) { // проверка на правильный ввод аргументов
+                if (!isdigit(expression[j]) && expression[j] != '+' && expression[j] != '-' && expression[j] != '(' && expression[j] != ')') {
                     check = false;
                     break;
                 }
-                i++;
+                j++;
             }
-            if (check == true) cout << "Результат: " << calculate(expression) << endl; // Вывод результата, если всё введено верно
-            else cout << "Ошибка в вводе аргументов!" << endl; // Иначе вывод ошибки
+            if (check == true) cout << "Результат: " << calculate(expression) << endl << endl; // Вывод результата, если всё введено верно
+            else cout << "Ошибка в вводе аргументов!" << endl << endl; // Иначе вывод ошибки
         }
     }
+
     return 0;
 }
